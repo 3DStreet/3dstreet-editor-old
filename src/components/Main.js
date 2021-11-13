cf2fc862d9f71fa187b0996947591d97d7816ccf
Kieran Farr <kieran.farr@gmail.com>
2021-11-12 22:23:47 -0800

    prevent logo wrap

--- src/components/Main.js
+++ src/components/Main.js
@@ -171,11 +171,11 @@ export default class Main extends React.Component {
 
           <div id="viewportBar">
           
-			  <a href="https://3d.st"><h1 style={{color: "#4B3461", fontSize: "18px", padding: "10px"}}>3D Street</h1></a>
+			  <a href="https://3d.st"><h1 style={{color: "#4B3461", fontSize: "18px", padding: "10px", "white-space": "nowrap"}}>3D Street</h1></a>
 		    <CameraToolbar />
 		
-            {/* <ViewportHUD />
-            <TransformToolbar /> */}
+            {/* <ViewportHUD /> */}
+            {/* <TransformToolbar /> */}
           </div>
 
           {/* <div id="rightPanel">
