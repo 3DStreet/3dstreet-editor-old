89221dd413edbb55ed61202d4d77793317e3a74a
Kieran Farr <kieran.farr@gmail.com>
2021-11-12 20:11:20 -0800

    change text color to #4B3461 per figma

--- src/components/Main.js
+++ src/components/Main.js
@@ -171,7 +171,7 @@ export default class Main extends React.Component {
 
           <div id="viewportBar">
           
-			<a href="https://3d.st"><p style={{fontSize: "18px", padding: "10px"}}>3D Street</p></a>
+			  <a href="https://3d.st"><h1 style={{color: "#4B3461", fontSize: "18px", padding: "10px"}}>3D Street</h1></a>
 		    <CameraToolbar />
 		
             {/* <ViewportHUD />
